74371f4868bcdb8337f982dd87a26605fae2c6ae
Hamed khosrawi <khosrawi.hame2@gmail.com>
2023-06-11 20:27:44 +0330

    Update AbstractEntity.java

--- src/main/java/org/acme/entity/AbstractEntity.java
+++ src/main/java/org/acme/entity/AbstractEntity.java
@@ -10,7 +10,7 @@ import java.time.Instant;
 @Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-<<<<<<< HEAD
+
 public abstract class  AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,18 +20,5 @@ public abstract class  AbstractEntity {
 
     @Column(name = "last_modified_date")
     private Instant lastModifiedDate;
-=======
-public abstract class AbstractEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @Column(name = "create_date", nullable = false)
-    private Instant createDate;
-
-    @Column(name = "last_modified_date")
-    private Instant lastModifiedDate;
-
->>>>>>> 4a507626c6b1199483a45b19f2457dadbd61a2db
 }
