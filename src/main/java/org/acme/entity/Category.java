fdb16c73485f83cccd002792148d1c09d11ec43e
root <khosrawi.hame2@gmail.com>
2023-06-11 12:42:27 +0330

    some classes added

---
+++ src/main/java/org/acme/entity/Category.java
@@ -0,0 +1,40 @@
+package org.acme.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@Entity
+@Table(name = "categories")
+public class Category extends AbstractEntity {
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name) &&
+                Objects.equals(description, category.description);
+
+    }
+
+    public int hashCode(){
+        return Objects.hash(name, description);
+    }
+}
---
+++ src/main/java/org/acme/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package org.acme.repositories;
+
+import org.acme.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
