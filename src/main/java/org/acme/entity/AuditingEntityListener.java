0b07ae74c49d261f819eaf3ad88d92e33ac8e59f
Hamed khosrawi <khosrawi.hame2@gmail.com>
2023-06-11 20:28:34 +0330

    Update AuditingEntityListener.java

--- src/main/java/org/acme/entity/AuditingEntityListener.java
+++ src/main/java/org/acme/entity/AuditingEntityListener.java
@@ -6,25 +6,13 @@ import jakarta.persistence.PreUpdate;
 import java.time.Instant;
 
 public class AuditingEntityListener {
-<<<<<<< HEAD
 
-    @PrePersist
-    void preCreate(AbstractEntity auditable){
-
-        Instant now = Instant.now();
-        auditable.setCreatedDate(now);
-        auditable.setLastModifiedDate(now);
-
-    }
-
-=======
     @PrePersist
     void preCreate(AbstractEntity auditable){
         Instant now = Instant.now();
         auditable.setCreateDate(now);
         auditable.setLastModifiedDate(now);
     }
->>>>>>> 4a507626c6b1199483a45b19f2457dadbd61a2db
     @PreUpdate
     void preUpdate(AbstractEntity auditable){
         Instant now = Instant.now();
